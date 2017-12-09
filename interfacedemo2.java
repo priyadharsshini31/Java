5cab32b0b7ec716b366b4fb522b69285055cd50a
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2017-12-09 12:16:31 +0530

    default method with body inside interface

---
+++ interfacedemo2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+interface demo1
+{
+	int i=10;
+	void demo_display();
+	default void must_display()
+	{
+		System.out.println("thanks for visiting");
+	}
+	
+}
+public class interface_demo implements demo1 {
+
+	public void demo_display()
+	{
+		
+		System.out.println("Iam the definition for the display interface"+i);
+		
+	}
+	public static void main(String[] args)
+	{
+		interface_demo id=new interface_demo();
+		id.demo_display();
+		id.must_display();
+	}
+
+}
