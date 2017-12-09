34f6fc0883477a31c60dd29b63a41976558dc664
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2017-12-09 12:10:07 +0530

    simple demo explaining interface

---
+++ inetrfacedemo1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+interface demo1
+{
+	int i=10;
+	void demo_display();
+	
+}
+public class interface_demo implements demo1 {
+	public void demo_display()
+	{
+		System.out.println("Iam the definition for the display interface");
+		
+	}
+	public static void main(String[] args)
+	{
+		interface_demo id=new interface_demo();
+		id.demo_display();
+	}
+
+}
