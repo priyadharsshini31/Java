6577acdf2efe93d2028e4b4513bf93e9c46a8800
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2018-02-04 09:36:25 +0530

    arrays to hold different data types

---
+++ Array/Differentdatatypes.java
@@ -0,0 +1,16 @@
+package Multithreading;
+
+public class object {
+	public static void main(String[] args)
+	{
+		Object[] ob = new Object[4];
+		ob[1]="priya";
+		ob[2]=100;
+		ob[3]=3.14;
+		System.out.println("The data type is"+ob[1].getClass());
+		System.out.println("The datatype is "+ob[2].getClass());
+		System.out.println("The datatype is "+ob[3].getClass());
+	
+
+}
+}
